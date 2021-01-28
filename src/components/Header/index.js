d91d2b2bc8a66c2f4dce4d1bdbb8aeab0fed2119
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-28 12:56:32 -0300

    components and containers

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <Header>   
+            <nav>
+                <a href="https://github.com/jhonataT/blog-react/blob/master/src/App.js">Home</a>
+                <a href="https://github.com/jhonataT/blog-react/blob/master/src/App.js">About Us</a>
+                <a href="https://github.com/jhonataT/blog-react/blob/master/src/App.js">Contact Us</a>
+            </nav>
+        </Header>
+    );
+};
+
+export default Header;
---
+++ src/containers/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import Header from '../../components/Header';
+
+const Home = props => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+};
+
+export default Home;
+
