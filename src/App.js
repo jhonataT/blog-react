9f444045897f8d649f0eb3d078e93e67de077d6f
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-30 18:55:03 -0300

    Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from './containers/Home';
 import './App.css';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 const App = () => {
     return (
         <div className="App">
             <Header/>
+            <Hero/>
             <Home/>
         </div>
     );
---
+++ src/components/Hero/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../UI/Card';
+import './style.css';
+
+const Hero = props => {
+    return (
+        <div>
+            <Card>
+                Jhonata Tenorio
+            </Card>
+        </div>
+    );
+};
+
+export default Hero;
