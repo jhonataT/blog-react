3d278008751210675d41def30e74d51f5a869599
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-04 15:34:33 -0300

    logo in navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import searchIcon from '../../assets/icons/search-32.png'; 
 import './style.css';
 
 const Navbar = props => {
     return (
-        <div>
-            Navbar
+        <div className="navbar">
+            <ul className="navbarMenu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Posts</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+            <div className="search">
+                <input type='text' placeholder="Search" />
+                <img src={searchIcon} alt='Search' />
+            </div>
         </div>
     );
 };
