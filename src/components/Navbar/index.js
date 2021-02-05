00ceae8f8af2a728a4539c8d1ec5ae4c2ec82ecc
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-05 11:14:21 -0300

    form styled

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import searchIcon from '../../assets/icons/search-32.png'; 
 import './style.css';
 
 const Navbar = props => {
+
+    const [search, setSearch] = useState(false);
+
+    const submiteSearch = e => {
+        e.preventDefault();
+        alert('Searched');
+    }
+
+    const openSearch = () => {
+        setSearch(true);
+    }
+
+    const searchClass = search ? 'searchInput active' : 'searchInput';
+
     return (
         <div className="navbar">
             <ul className="navbarMenu">
@@ -12,8 +26,10 @@ const Navbar = props => {
                 <li><a href="#">Contact Us</a></li>
             </ul>
             <div className="search">
-                <input type='text' placeholder="Search" />
-                <img src={searchIcon} alt='Search' />
+                <form onSubmit={submiteSearch}>
+                    <input className={searchClass} type='text' placeholder="Search" />
+                    <img onClick={openSearch} className="searchIcon" src={searchIcon} alt='Search' />
+                </form>
             </div>
         </div>
     );
