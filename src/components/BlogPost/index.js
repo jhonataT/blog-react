bd43e1f3319edcb988bac10005f68bbc1520e5c4
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-13 10:43:16 -0300

    BlogPost

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import Card from '../UI/Card';
+import blogPost from '../../data/blog.json';
 
-const BlogPost = props => {
+const BlogPost = (props) => {
+
+    const [post, setPost] = useState({
+        id: "",
+        blogCategory: "",
+        blogTitle: "",
+        postedOn: "",
+        author: "",
+        blogImage: "",
+        blogText: ""
+    });
+    const [slug, setSlug] = useState('');
+
+
+    useEffect(() => {
+        const slug = props.match.params.slug;
+        const post = blogPost.data.find(post => post.slug == slug);
+        setPost(post);
+        setSlug(slug)
+    }, [post, props.match.params.slug]);
+
+    if (post.blogImage == "") return null;
+
     return (
-        <Card width='70%'>
-            
-        </Card>
-    );
-};
+        <div className="blogPostContainer">
+            <Card>
+                <div className="blogHeader">
+                    <span className="blogCategory">{post.blogCategory}</span>
+                    <h1 className="postTitle">{post.blogTitle}</h1>
+                    <span className="postedBy">posted on {post.postedOn} by {post.author}</span>
+                </div>
 
-export default BlogPost;
+                <div className="postImageContainer">
+                    <img src={require('../../blogPostImages/' + post.blogImage)} alt="Post Image" />
+
+                </div>
+
+                <div className="postContent">
+                    <h3>{post.blogTitle}</h3>
+                    <p>{post.blogText}</p>
+                </div>
+
+            </Card>
+        </div>
+    )
+
+}
+
+export default 
