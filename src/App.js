1d7614a54ddc0e5149bc82701f8ba35e09831565
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-13 10:37:44 -0300

    containers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import Home from './containers/Home';
-import './App.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import ContactUS from './containers/ContactUS';
-import Post from './containers/Post';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Home from "./containers/Home";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ContactUS from "./containers/ContactUS";
+import Post from "./containers/Post";
 
-const App = () => {
-    return (
-        <Router>
-            <div className="App">
-                <Header/>
-                <Hero/>
-                <Route path="/" exact component={Home} />
-                <Route path="/contact-us" component={ContactUS} />
-                <Route path="/post" component={Post} />
-            </div>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Hero />
+        <Route path="/" exact component={Home} />
+        <Route path="/contact-us" component={ContactUS} />
+        <Route path="/post/:slug" component={Post} />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
--- src/containers/Post/index.js
+++ src/containers/Post/index.js
@@ -3,14 +3,16 @@ import './style.css';
 import Card from '../../components/UI/Card';
 import BlogPost from '../../components/BlogPost';
 import Sidebar from '../../components/Sidebar';
+import Layout from '../../components/Layout';
 
-const Post = props => {
-    return (
-        <section className="container">
-            <BlogPost />
-            <Sidebar />
-        </section>
-    );
-};
+const Post = (props) => {
+    console.log(props);
+  return(
+        <Layout>
+          <BlogPost {...props} />
+        </Layout>
+   )
+
+ }
 
 export default Post;
